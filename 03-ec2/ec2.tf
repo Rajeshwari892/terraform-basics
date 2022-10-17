92a6b702b9c1e548958baba6d4d0c08a312ffbcf
Rajeshwari <arajeshwari2022@gmail.com>
2022-10-18 00:16:38 +0530

    ec2 instance

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "demo" {
   ami           = "ami-00ff427d936335825"
   instance_type = "t2.micro"
@@ -8,3 +12,7 @@ resource "aws_instance" "demo" {
     Name = "linux-terraform"
   }
 }
+
+
+# Note:
+# attach iam role to the instance om aws machine to authenticate to provide the infra
