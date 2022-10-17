ec87ac56276bd9ef0f42125a9879427a1cf41293
Rajeshwari <arajeshwari2022@gmail.com>
2022-10-18 01:32:59 +0530

    ec2 instance

--- remote-terraform/backend.tf
+++ remote-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "b50-cl-terraform-state-bucket"
-    key    = "test/env/my/key"
+    key    = "test/env/terraform.tfstate"
     region = "us-east-1"
   }
 }
