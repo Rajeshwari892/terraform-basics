7ea8271216c02598eec9916aa3c9be7bdf7301ea
Rajeshwari <arajeshwari2022@gmail.com>
2022-10-17 23:31:54 +0530

    terraform vars  module added

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -5,3 +5,18 @@ variable "test"{
 output "sample" {
     value = var.test
 }
+
+output "sample-op1" {
+    value = "Value is ${var.sample}"
+}
+
+# A variable can be accessed with or without ${} only if the variable is called. But in case if it is needed to add with some other strings then we need to use double
+# There is no concept of single quores in terraform.
+
+variable "number" {
+    default = 100 
+}
+
+output "number" {
+    value = var.number  
+}
